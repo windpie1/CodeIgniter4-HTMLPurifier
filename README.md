a9a9d62b0a36cfab473e6e959c1dedccc146d932
WindPie <wzf28@sina.com>
2022-01-02 22:35:33 +0800

    Fixed: It's now for CodeIgniter 4.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-CodeIgniter HTML Purifier Helper
+CodeIgniter4 HTML Purifier Helper
 -------------------------------
 Purify input using the HTML Purifier standalone class.  
 Easily use multiple HTML Purifier configurations.
@@ -6,7 +6,7 @@ Easily use multiple HTML Purifier configurations.
 Installation
 ------------
 Copy HTML Purifier helper to:  
-`./application/helpers/htmlpurifier_helper.php`
+`./app/Helpers/htmlpurifier_helper.php`
 
 Download [HTML Purifier](http://htmlpurifier.org/download) with composer, like so:  
 `composer require ezyang/htmlpurifier`
@@ -16,7 +16,7 @@ Usage
 Create and customize HTML Purifier configurations by opening the helper (`htmlpurifier_helper.php`) and editing/adding case statements as needed. There's a default configuration named "comment" that should be sufficient for basic web comments with HTML allowed, but be sure to check it and make sure it meets your standards. View the [HTML Purifier Configuration Documentation](http://htmlpurifier.org/live/configdoc/plain.html) for more information.
 
 Use the following code to purify using the default configuration:  
-`$this->load->helper('htmlpurifier');`  
+`helper('htmlpurifier');`  
 `$clean_html = html_purify($dirty_html);`  
 Where `$dirty_html` is a string, or an array of strings.
 
--- htmlpurifier_helper.php
+++ htmlpurifier_helper.php
@@ -1,14 +1,10 @@
 <?php
 
-defined('BASEPATH') or exit('No direct script access allowed');
-
-/*
- * Codeigniter HTMLPurifier Helper
- *
+/**
  * Purify input using the HTMLPurifier standalone class.
  * Easily use multiple purifier configurations.
  *
- * @author     Tyler Brownell <tyler.brownell@mssociety.ca>
+ * @author     Tyler Brownell <tyler.brownell@mssociety.ca>, Windpie <wzf28@sina.com>,
  * @copyright  Public Domain
  *
  * @access  public
@@ -16,7 +12,18 @@ defined('BASEPATH') or exit('No direct script access allowed');
  * @param   string           $config      The name of the configuration (switch case) to use.
  * @return  string or array               The cleaned string (or array of strings).
  */
-if (!function_exists('html_purify')) {
+
+// CodeIgniter 4 HTMLPurifier Helper
+
+if (! function_exists('html_purify')) {
+    /**
+     *
+     * @access  public
+     * @param array|string $dirty_html      A string (or array of strings) to be cleaned.
+     * @param string $config                The name of the configuration (switch case) to use.
+     * @return array|string                 The cleaned string (or array of strings).
+     */
+
     function html_purify($dirty_html, $config = false)
     {
         if (is_array($dirty_html)) {
@@ -24,12 +31,11 @@ if (!function_exists('html_purify')) {
                 $clean_html[$key] = html_purify($val, $config);
             }
         } else {
-            $ci = &get_instance();
-
+            $charset = config('app')->charset;
             switch ($config) {
                 case 'comment':
                     $config = \HTMLPurifier_Config::createDefault();
-                    $config->set('Core.Encoding', $ci->config->item('charset'));
+                    $config->set('Core.Encoding', $charset);
                     $config->set('HTML.Doctype', 'XHTML 1.0 Strict');
                     $config->set('HTML.Allowed', 'p,a[href|title],abbr[title],acronym[title],b,strong,blockquote[cite],code,em,i,strike');
                     $config->set('AutoFormat.AutoParagraph', true);
@@ -39,12 +45,12 @@ if (!function_exists('html_purify')) {
 
                 case false:
                     $config = \HTMLPurifier_Config::createDefault();
-                    $config->set('Core.Encoding', $ci->config->item('charset'));
+                    $config->set('Core.Encoding', $charset);
                     $config->set('HTML.Doctype', 'XHTML 1.0 Strict');
                     break;
 
                 default:
-                    show_error('The HTMLPurifier configuration labeled "'.htmlspecialchars($config, ENT_QUOTES, $ci->config->item('charset')).'" could not be found.');
+                    show_error('The HTMLPurifier configuration labeled "'.htmlspecialchars($config, ENT_QUOTES, $charset).'" could not be found.');
             }
 
             $purifier = new \HTMLPurifier($config);
@@ -54,6 +60,3 @@ if (!function_exists('html_purify')) {
         return $clean_html;
     }
 }
-
-/* End of htmlpurifier_helper.php */
-/* Location: ./application/helpers/htmlpurifier_helper.php */
